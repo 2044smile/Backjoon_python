3f1dc0a5badd3f7bbf7a1efbd6d2ce2dd6a4d5fd
2044smile <2044smile@naver.com>
2023-10-20 14:21:11 +0900

    :sparkles: Feat: 두 배열의 원소 교체
    자주 등장하는 문제 a[i], b[i] = b[i], a[i]

--- ndb796/python-for-coding-test/CHAPTER_06/두_배열의_원소_교체.py
+++ ndb796/python-for-coding-test/CHAPTER_06/두_배열의_원소_교체.py
@@ -7,9 +7,17 @@
 출력 조건
 최대 K번의 바꿔치기 연산을 수행하여 만들 수 있는 배열 A의 모든 원소의 합의 최댓값을 출력한다.
 """
-n, k = input().split()
+n, k = map(int, input().split())
 
 a = list(map(int, input().split()))
+a.sort()  # 오름차순 정렬
 b = list(map(int, input().split()))
+b.sort(reverse=True)  # 내림차순 정렬
 
-print(a, b)
+for i in range(k):
+    if a[i] < b[i]:  # a=1,2,3 b=6,6,5
+        a[i], b[i] = b[i], a[i]
+    else:
+        break
+
+print(sum(a))
